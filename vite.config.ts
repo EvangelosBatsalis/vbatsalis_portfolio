f6e95b3119e1489166b1de6b5411fe34931c8420
Evangelos Batsalis <47661387+EvangelosBatsalis@users.noreply.github.com>
2026-01-05 16:24:34 +0200

    Update allowedHosts in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   preview: {
     host: true,
     port: 3000,
     strictPort: true,
-    allowedHosts: true,
+    allowedHosts: ["vbatsalis-web", "vbatsalis.gr", "www.vbatsalis.gr"],
   },
 });
