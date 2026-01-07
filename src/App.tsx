cbcd1d66a4800f7fa891b88cb5c2ab783b934874
Evangelos Batsalis <ebatsalis@scl.local>
2026-01-08 01:29:58 +0200

    changed the tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Portfolio from './components/Portfolio';
 
 export default function App() {
-  return 
-    <>
-    <title>vbatsalis_portfolio - DEPLOY c4b638a</title>
-    <Portfolio />;
-    </>
+  return (
+  <>  
+  <Portfolio />
+  </>
+  );
 }
